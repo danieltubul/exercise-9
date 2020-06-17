86cc7541dec179c7c2f0d01072a7cdbd8aba5404
danieltubul <danieltouboul@gmai.com>
2020-06-17 20:14:36 +0300

    toPush

--- src/Backend/srv.py
+++ src/Backend/srv.py
@@ -17,6 +17,10 @@ app = Flask(__name__,
             static_folder='../Frontend/build',
             static_url_path='/')
 
+@app.route("/api/alive")
+def alive():
+    return "alive"
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
